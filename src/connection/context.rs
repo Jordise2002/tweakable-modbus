2a6b6129f86e74ce4bbe5d00a419873f5587e1eb
Jordi <jordisegura2002@gmail.com>
2025-05-14 20:01:28 +0200

    added subprotocol

--- src/connection/context.rs
+++ src/connection/context.rs
@@ -1,5 +1,6 @@
 
+//This struct is meant to hold the state of the on going modbus communication
 pub struct ModbusContext
 {
-    
+
 }
--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -4,8 +4,15 @@ use socket::ModbusSocket;
 mod context;
 mod socket;
 
+pub enum ModbusSubprotocol {
+    ModbusTCP,
+    ModbusRTU,
+    ModbusRTUOverTCP
+}
+
 pub struct ModbusConnection
 {
     comm: Box<dyn ModbusSocket>,
-    context: ModbusContext
+    context: ModbusContext,
+    subprotocol: ModbusSubprotocol
 }
